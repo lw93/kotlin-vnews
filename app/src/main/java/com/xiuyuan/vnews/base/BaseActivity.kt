3f0e55b8865e6e16ded0d0c4dfaad142bcd56320
lw93 <842363736@qq.com>
2018-05-07 13:06:09 +0800

    更新状态栏

--- app/src/main/java/com/xiuyuan/vnews/base/BaseActivity.kt
+++ app/src/main/java/com/xiuyuan/vnews/base/BaseActivity.kt
@@ -1,7 +1,10 @@
 package com.xiuyuan.vnews.base
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.xiuyuan.vnews.R
 import com.xiuyuan.vnews.utils.ToastUtil
 import org.greenrobot.eventbus.EventBus
 
